1868a4fab46d90efcb73d3cf50ff136cd468eef7
LaramieCode <LaramieCode@gmail.com>
2022-08-19 13:36:51 -0500

    add html and js files and add pseudocode

---
+++ javascript.js
@@ -0,0 +1,6 @@
+console.log("Hello World")
+
+//make a function that makes you choose Rock Paper or Scissors.
+//make a function that randomly chooses Rock Paper or Scissors.
+//make a function that finds the winner of you vs random, and return the winner
+//make a function that shows the winner in console.log
