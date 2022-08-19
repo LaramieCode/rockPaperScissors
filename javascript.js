f28172ca66c94c0954101dc488b4b7a01c13aa67
LaramieCode <LaramieCode@gmail.com>
2022-08-19 14:02:47 -0500

    add getComputerChoice function

--- javascript.js
+++ javascript.js
@@ -7,5 +7,21 @@ function yourChoice () {
     return(choice.toLowerCase())
 }
 //make a function that randomly chooses Rock Paper or Scissors.
+let random;
+function getComputerChoice () {
+    n = Math.floor(Math.random() * 3) //Chooses a random number 0 - 3
+    switch (n) {
+        case 0:
+            random = "rock"
+            break;
+        case 1:
+            random = "paper"
+            break;
+        case 2:
+            random = "scissors"
+            break;
+    }
+    return random
+}
 //make a function that finds the winner of you vs random, and return the winner
 //make a function that shows the winner in console.log
